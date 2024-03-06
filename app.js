f68f014ac153d97633df97cf493f3cc48516e59e
hien nguyen <thuhiennguyen198@gmail.com>
2024-03-06 14:44:27 -0500

    connect to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const keys = require("./config/keys")
+const mongoose = require("mongoose");
+const { MONGO_URL } = process.env;
+// Make sure the database is connected before starting the server
 
+
+console.log({keys});
 app.get('/', (req, res) => {
     res.send("This is a blank home page")
 })
@@ -12,19 +19,38 @@ app.get('/createToken', (req,res) => {
         userName: "admin",
         email: "admin@gamil.com",
     }
-    const token = jwt.sign({ user: user }, process.env.SECRET_KEY);
+    const token = jwt.sign({ user: user }, keys.secretOrKey);
     console.log({token});
+    //tao duoc token roi thi gui ve luu o client
     res.send("Token was created successfully")
 })
 
-app.get('/profile', (req,res) => {
+app.get('/profile', async (req,res) => {
+    const result = await jwt.verify(
+
+    )
     res.send("This is profile page")
 })
 
 app.get("/wrongToken", (req, res) => {
   res.send("Wrong token");
 });
+const port = process.env.PORT || 5000;
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
-})
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+    // Start your Express server or perform other operations
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+// app.listen(3000, () => {
+//     console.log("Listening on port 3000");
+// })
+
+// Bản chất của authentication là xác định tính hợp lệ của 1 yêu cầu gửi tới gồm: 
+// - tính chính danh của người gửi
+// - tính toàn vẹn của dữ liệu trên đường truyền dẫn yêu cầu tới đích.
+//nguoi dung login => request => server => authen => gui lai 1 token luu o client => client gui kem token o moi request de server co the xac thuc+tra lai data
